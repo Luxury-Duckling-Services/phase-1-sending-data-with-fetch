9f29f5e8dc3d3b5747ba2b05ac4e4ab3cf5747b0
Yifan <datasciencewyfan@gmail.com>
2022-05-24 16:06:07 -0700

    commit tree ae1192e76dc59f07158068e4aab7273ca56dd3c7

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+const body = document.getElementsByTagName('body')[0]
+
+const submitData = (userName , userEmail) => {
+    const formData = {
+        name: userName,
+        email: userEmail,
+      };
+      
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
+      
+    return fetch("http://localhost:3000/users", configurationObject)
+    
+    .then(function (response) {
+        return response.json();
+    })
+    
+    .then(function (object) {
+        body.innerHTML += object.id.toString()
+    })
+
+    .catch(function (error) {
+        body.innerHTML += error.message.toString()
+    })
+    
+}
+
